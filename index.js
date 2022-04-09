14c7df1592db792a5eda8c6bc7b116606f9dfcdc
Pufikas <dominykas.guzys@gmail.com>
2022-04-09 17:11:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ let scoreCounter = document.getElementById('score')
 let images = ['Fumos/1 (1).jpg', 'Fumos/1 (2).jpg', 'Fumos/1 (3).jpg','Fumos/1 (4).jpg','Fumos/1 (5).jpg','Fumos/1 (6).jpg','Fumos/1 (7).jpg','Fumos/1 (8).jpg','Fumos/1 (9).jpg','Fumos/1 (10).jpg','Fumos/1 (11).jpg','Fumos/1 (12).jpg','Fumos/1 (13).jpg','Fumos/1 (14).jpg','Fumos/1 (15).jpg','Fumos/1 (16).jpg','Fumos/1 (18).jpg','Fumos/1 (19).jpg','Fumos/1 (20).jpg','Fumos/1 (21).jpg','Fumos/1 (22).jpg','Fumos/1 (23).jpg','Fumos/1 (24).jpg','Fumos/1 (25).jpg','Fumos/1 (26).jpg','Fumos/1 (27).jpg','Fumos/1 (28).jpg','Fumos/1 (29).jpg','Fumos/1 (30).jpg','Fumos/1 (31).jpg','Fumos/1 (32).jpg','Fumos/1 (33).jpg','Fumos/1 (34).jpg',]
 let index = 0
 
+
+
+
 function startGame() {
   console.log('Started')
   startButton.classList.add('hide')
   shuffledQuestions = questions.sort(() => +1 )
   currentQuestionIndex = 0
   currentImageIndex = 0
+  score = 0
   questionContainerElement.classList.remove('hide')
   setNextQuestion()
   }
@@ -59,10 +63,9 @@ function showQuestion(question) {
     button.classList.add('btn')
     if (answer.correct) {
       button.dataset.correct = answer.correct
-      let score = 0
-      score++
-      scoreCounter.innerText = "Score " + score
-      console.log(score)
+      // score++
+      // scoreCounter.innerText = 'Score ' + score
+      // console.log(score)
     }
     button.addEventListener('click', selectAnswer)
     answerButtonsElement.appendChild(button)
@@ -97,10 +100,19 @@ function setStatusClass(element, correct) {
   clearStatusClass(element)
   if (correct) {
     element.classList.add('correct')
+    // scorePlus()
   } else {
     element.classList.add('wrong')
   }
 }
+
+function scorePlus() {
+      score++
+      scoreCounter.innerText = 'Score ' + score
+      console.log(score)
+  } 
+
+
 
 function clearStatusClass(element) {
   element.classList.remove('correct')
